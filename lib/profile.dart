c932035ffa25b6ccb07bd858e26fc7a68e4da4db
qamar369 <qamarubaid77@gmail.com>
2025-09-13 03:58:40 +0530

    complete

--- lib/profile.dart
+++ lib/profile.dart
@@ -23,32 +23,95 @@ class Profile extends StatelessWidget {
             topRight: Radius.circular(12),
           ),
         ),
-        child: Column(
-          children: [
-            SizedBox(height: 20
-          ,),
-            CircleAvatar(
-              backgroundColor: Colors.purple,
-              child: Icon(Icons.camera_alt,color: Colors. grey,),
-              radius: 30,
-            ),
-            Text(
-              "Qamar",
-              style: TextStyle(
-                color: Colors.purple,
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              CircleAvatar(
+                backgroundColor: Colors.purple,
+                child: Icon(Icons.camera_alt, color: Colors.grey),
+                radius: 30,
               ),
-            ),
-            Text(
-              "Flutter Developer",
-              style: TextStyle(
-                color: Colors.grey,
-                fontSize: 21,
-                fontWeight: FontWeight.bold,
+              Text(
+                "Qamar",
+                style: TextStyle(
+                  color: Colors.purple,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-          ],
+              Text(
+                "Flutter Developer",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 21,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 12),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Your Email",
+                  style: TextStyle(color: Colors.black, fontSize: 12),
+                ),
+              ),
+              SizedBox(
+                height: 55,
+                width: double.infinity,
+                child: Card(
+                  color: Colors.grey,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [Text("qamarubaid77@gmail.com"), Icon(Icons.email)]),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "phone number",
+                  style: TextStyle(color: Colors.black, fontSize: 12),
+                ),
+              ),
+              SizedBox(
+                height: 55,
+                width: double.infinity,
+                child: Card(
+                  color: Colors.grey,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),   child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [Text("9947559167"), Icon(Icons.phone)]),
+                  ),
+                ),
+              ),
+              Align(alignment: Alignment.topLeft, child: Text("place")),
+              SizedBox(
+                height: 55,
+                width: double.infinity,
+                child: Card(color: Colors.grey,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [Text("payyoli"), Icon(Icons.place)]),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
